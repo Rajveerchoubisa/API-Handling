e2d583efa5a9aa7365adb50dce13791dd7ae76e0
Rajveer Choubisa <ranveer0596@gmail.com>
2025-02-04 16:10:34 +0530

    requests, axios

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -6,35 +6,58 @@ function App() {
   const [products,setProducts] = useState([])
   const [error,setError] = useState(false)
   const [loading , setLoading] = useState(false)
+  const [search,setSearch] = useState('')
 
   useEffect(() => {
-    (async () => {
+    const controller = new AbortController()
+    ;(async () => {
       try {
         setLoading(true)
         setError(false)
-        const result = await axios.get('http://localhost:3000/api/products')  // Use full URL
+        const result = await axios.get('http://localhost:3000/api/products?search=' + search,{
+          signal: controller.signal
+        })  // Use full URL
         console.log(result.data);
         setProducts(result.data);
         setLoading(false)
       } catch (error) {
+        if(axios.isCancel(error)){
+          console.log('Request canceled', error.message)
+        }
         setError(true)
         setLoading(false)
       }
     })();
-  }, []);
 
-  if(error){
-    return <h1>Something went wrong</h1>
-  }
+    //cleanup code 
+    return () => {
+            controller.abort()
+    }
+  }, [search]);
 
-  if(loading){
-    return <h1>Loading ...</h1>
-  }
+  // if(error){
+  //   return <h1>Something went wrong</h1>
+  // }
+
+  // if(loading){
+  //   return <h1>Loading ...</h1>
+  // }
   
 
   return (
     <>
      <h1> Learning API </h1>
+     <input type='text' placeholder='search'
+     value={search}
+     onChange={(e) => setSearch(e.target.value)} // performance of application will be down as network tab will update eeach query and raise the input/query
+      // so we use use axios to cancel the old request
+
+     />
+
+     {loading && (<h1> Loading ...</h1>)}
+     {error && (<h1> Something went wrong </h1>)}
+
+
      <h2>Number of products are: {products.length}</h2>
     </>
   )
